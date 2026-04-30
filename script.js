6a442133dcdba9997ae5530bba14586f475f0959
sjcchub <maxgeorgebro@gmail.com>
2026-04-30 22:55:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Convert to ES5 syntax
-function showGenerateBtn() {
-    var generateBtn = document.getElementById('generateBtn');
-    if (generateBtn) {
-        generateBtn.style.display = 'block';
-    }
-}
